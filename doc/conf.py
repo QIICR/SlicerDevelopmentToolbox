c27a3b649433cecb1f41d8ddf1f11b1754baf878
Christian Herz <christianherz1985@googlemail.com>
2017-06-02 15:03:28 -0400

    ENH: added documentation for buttons and improved code

--- doc/conf.py
+++ doc/conf.py
@@ -66,7 +66,7 @@ sys.modules['vtk.vtkCommand.UserEvent'] = 1
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -110,7 +110,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+todo_include_todos = True
 
 
 # -- Options for HTML output ----------------------------------------------
